3fbf2fb1b8165233ada6c8432e702baa1a3c3ab3
Jay Gupta <jaygupta010203@gmail.com>
2020-07-15 16:48:01 +0530

    Inital Commit

---
+++ bs4-tut.py
@@ -0,0 +1,7 @@
+import bs4 as bs
+import urllib.request
+
+source = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
+soup = bs.BeautifulSoup(source,'lxml')
+
+print(soup.encode())
